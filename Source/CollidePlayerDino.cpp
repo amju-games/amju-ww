6824ec6e6117d429a98bb246c02185557155b398
jason <jason@amju.com>
2009-08-25 22:08:20 +0000

    Try to prevent player and dino getting stuck together

--- Source/CollidePlayerDino.cpp
+++ Source/CollidePlayerDino.cpp
@@ -1,28 +1,31 @@
 #include "CollisionManager.h"
 #include "Player.h"
 #include "Dino.h"
+#include "SoundManager.h"
 
 namespace Amju
 {
 void CollidePlayerDino(GameObject* go1, GameObject* go2)
 {
-  // TODO Sound effect
+  // Sound effect
+  // TODO Make sure not played more than once in successive frames
+  // -- have a timer
+  TheSoundManager::Instance()->PlayWav("gasp"); // NB No file ext
 
-  // Collision like pool balls. We work out the total velocity of both
-  // objects, projected onto the line connecting their centres.
-  // Each object gets this vector added to their velocity.
-  // We may want to damp this a bit.
-  static const float DAMP = 0.5f; // TODO CONFIG
+  // Push both objects away from each other
+
+  static const float RESPONSE = 10.0f; // TODO CONFIG
 
   // Line between centres
   Vec3f line = go1->GetPos() - go2->GetPos();
-  Vec3f totalVel = go1->GetVel() - go2->GetVel();
-  // go1 gets new vel = go1->vel + totalVel projected onto line
-  float invSqLen = 1.0f / line.SqLen();
-  Vec3f proj = line * (DotProduct(totalVel, line) * invSqLen) * DAMP;
-  //  = totalVel projected onto line 
-  go1->SetVel(go1->GetVel() - proj);
-  go2->SetVel(go2->GetVel() + proj);
+  line.Normalise();
+  line *= RESPONSE;
+
+  go1->SetVel(go1->GetVel() + line);
+  go1->SetAcc(-go1->GetVel());
+
+  go2->SetVel(go2->GetVel() - line);
+  go2->SetAcc(-go2->GetVel());
 }
 
 static bool b = TheCollisionManager::Instance()->Add(
