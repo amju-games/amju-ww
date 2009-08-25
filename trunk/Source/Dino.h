3571cf98a7caf8ffabce5e9e992e479328a3227e
jason@amju.com <jason@amju.com@347d3d46-8993-11de-a519-b78fc8232508>
2009-08-25 22:10:41 +0000

    Non-symmetrical AABB

--- trunk/Source/Dino.h
+++ trunk/Source/Dino.h
@@ -17,8 +17,14 @@ public:
   virtual bool Load(File*);
 
 protected:
+  // Dinos have big heads so AABB is not symmetrical
+  void UpdateAabb();
+
+protected:
   // Timer - must reach limit, then we can decide what direction to move in etc.
   float m_decideTime;
+  enum DinoType { AMJU_DINO_GREEN, AMJU_DINO_BLUE, AMJU_DINO_RED };
+  DinoType m_dinoType;
 };
 }
 
