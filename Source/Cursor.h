e953ae25c590c776049ce97f43882eca7bdb2829
jason <jason@amju.com>
2009-10-18 21:07:50 +0000

    Activate on first event; new coord system

--- Source/Cursor.h
+++ Source/Cursor.h
@@ -18,12 +18,15 @@ public:
   bool Load(int id);
   void Draw();
 
+  const Vec2f& GetPos() const;
+
 private:
   PTexture m_pTex;
   float m_rot; // rotation about z
   Vec2f m_pos;
 	AmjuGL::Tris m_tris;
   int m_id;
+  bool m_isActive;
 };
 } 
 
