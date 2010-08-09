c37132830712f962e984520b4399db8a37229a43
jason.e.colman <jason.e.colman@gmail.com>
2010-08-09 18:16:05 +0000

    Add iphone events

--- Source/iPhone/Classes/EAGLView.h
+++ Source/iPhone/Classes/EAGLView.h
@@ -14,7 +14,9 @@
 // This class wraps the CAEAGLLayer from CoreAnimation into a convenient UIView subclass.
 // The view content is basically an EAGL surface you render your OpenGL scene into.
 // Note that setting the view non-opaque will only work if the EAGL surface has an alpha channel.
-@interface EAGLView : UIView
+
+// j.c. added Accelerometer
+@interface EAGLView : UIView <UIAccelerometerDelegate> 
 {    
 @private
     id <ESRenderer> renderer;
