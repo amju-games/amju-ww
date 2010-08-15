c562173ea804b37ef6266cbb7e42ce1ff7e6b124
jason.e.colman <jason.e.colman@gmail.com>
2010-08-15 21:55:24 +0000

    Fixes for eating behaviour

--- Source/AIEatPet.cpp
+++ Source/AIEatPet.cpp
@@ -22,14 +22,27 @@ void AIEatPet::Update()
 {
   AI::Update();
 
+#ifdef _DEBUG
+  std::cout << "Dino eating pet!\n";
+#endif
+
   Vec3f v = m_npc->GetPos() - m_target->GetPos();
-  float degs = RadToDeg(atan2(v.z, v.x));
+  float degs = RadToDeg(atan2(-v.x, v.z)); // TODO ??
   m_npc->SetDir(degs);
 
   // TODO Wait till we are facing
 
   m_npc->SetAnim("eat");
   // TODO Eaten anim for pet, make dead after delay
-
+  m_target->SetVel(Vec3f(0, 0, 0)); // Stop pet moving
+  
+  if (m_time > 0.25f) // TODO CONFIG
+  {
+    ((Pet*)m_target)->SetDead(true);
+  }
+  if (m_time > 4.0f) // TODO CONFIG
+  {
+    m_npc->DecideAI();
+  }
 }
 }
