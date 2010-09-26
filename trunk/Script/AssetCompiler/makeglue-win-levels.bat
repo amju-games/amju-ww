b34db59fd89a1a73d189a12638b8c64696f30ba2
jason.e.colman@gmail.com <jason.e.colman@gmail.com@347d3d46-8993-11de-a519-b78fc8232508>
2010-09-26 17:26:55 +0000

    Patch level files in data.glue -- TODO makefile so only updated files get patched

---
+++ trunk/Script/AssetCompiler/makeglue-win-levels.bat
@@ -0,0 +1,36 @@
+REM makeglue.bat
+REM glue Levels only into a Glue file.
+
+set THIS_DIR=%cd%
+set TOP_DIR=%THIS_DIR%\..\..
+set COMPILED_ASSETS=%TOP_DIR%\Build\CompiledAssets
+set DEST_DIR=%COMPILED_ASSETS%\Win
+set SRC_DIR=%TOP_DIR%\Assets
+
+rem mkdir %COMPILED_ASSETS%
+rem mkdir %DEST_DIR%
+rem mkdir %DEST_DIR%\font2d
+rem del /S /Q %DEST_DIR%\*.*
+
+REM Copy other files to compiled dir
+REM /Y when we would get overwrite confirm prompt
+
+xcopy /Y /S %SRC_DIR%\levels\*.txt %DEST_DIR%\levels\
+
+cd %DEST_DIR%
+
+set GLUE_EXE=%THIS_DIR%\glue.exe
+set GLUE_FILE=%DEST_DIR%\..\data-win.glue
+REM DON'T trash glue file
+REM %GLUE_EXE% -c %GLUE_FILE%
+
+REM Add files in levels dir -- PATCH, so replace old versions
+for %%f in (levels\*.txt) do %GLUE_EXE% -p %GLUE_FILE% %%f
+
+
+REM Verify contents
+%GLUE_EXE% -d %GLUE_FILE%
+
+
+cd %THIS_DIR%
+
