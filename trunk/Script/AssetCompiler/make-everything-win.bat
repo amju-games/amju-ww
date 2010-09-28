d0a52226ec2e4eb614c7375d74b00d5976e819eb
jason.e.colman@gmail.com <jason.e.colman@gmail.com@347d3d46-8993-11de-a519-b78fc8232508>
2010-09-28 11:25:59 +0000

    Delete old files

--- trunk/Script/AssetCompiler/make-everything-win.bat
+++ trunk/Script/AssetCompiler/make-everything-win.bat
@@ -1,3 +1,13 @@
+set THIS_DIR=%cd%
+set TOP_DIR=%THIS_DIR%\..\..
+set COMPILED_ASSETS=%TOP_DIR%\Build\CompiledAssets
+set DEST_DIR=%COMPILED_ASSETS%\Win
+set SRC_DIR=%TOP_DIR%\Assets
+
+mkdir %COMPILED_ASSETS%
+mkdir %DEST_DIR%
+rem del /S /Q %DEST_DIR%\*.*
+
 REM Convert obj files to binary format, copy to compiled dir
 call MakeObjsBin-win.bat
 
