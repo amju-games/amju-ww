846706e0013d8da72085b92989be2757472b4189
jason.e.colman <jason.e.colman@gmail.com>
2010-09-29 16:54:11 +0000

    Add TutorialTrigger

---
+++ Source/TutorialTrigger.h
@@ -0,0 +1,24 @@
+#ifndef TUTORIAL_TRIGGER_H
+#define TUTORIAL_TRIGGER_H
+
+#include <string>
+#include "Trigger.h"
+
+namespace Amju
+{
+class TutorialTrigger : public Trigger
+{
+public:
+  static const char* NAME;
+  TutorialTrigger();
+  virtual const char* GetTypeName() const;
+  virtual bool Load(File*); 
+  virtual void OnPlayerCollision(Player*);
+
+private:
+  std::string m_text;
+  bool m_hasShownText;
+};
+}
+
+#endif
