efe5039a5b202cf7b3a15c50da550a8f57cd1e8c
jason.e.colman <jason.e.colman@gmail.com>
2011-03-17 17:15:07 +0000

    Menu fixes

--- Source/ContextMenu.cpp
+++ Source/ContextMenu.cpp
@@ -7,12 +7,6 @@ namespace Amju
 ContextMenu::ContextMenu()
 {
   m_isVisible = false;
-  TheEventPoller::Instance()->AddListener(this);
-}
-
-ContextMenu::~ContextMenu()
-{
-  TheEventPoller::Instance()->RemoveListener(this); 
 }
 
 void ContextMenu::Draw()
@@ -29,12 +23,15 @@ void ContextMenu::OnButtonEvent(const ButtonEvent& be)
 
 void ContextMenu::OnMouseButtonEvent(const MouseButtonEvent& mbe)
 {
+  // Do regular processing but reposition on R button
+  GuiMenu::OnMouseButtonEvent(mbe);
+
   if (mbe.button == AMJU_BUTTON_MOUSE_RIGHT &&
       mbe.isDown)
   {
     Cursor* c = TheCursorManager::Instance()->GetCursor(0);
     SetPos(c->GetPos());
-    //SetVisible(true);
+    SetVisible(true);
   }
 }
 }
