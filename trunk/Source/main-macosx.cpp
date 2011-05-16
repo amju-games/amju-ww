96ac02bb7a2086d9fa5a3852af5ee0e34fd357bd
jason.e.colman@gmail.com <jason.e.colman@gmail.com@347d3d46-8993-11de-a519-b78fc8232508>
2011-05-16 10:00:25 +0000

    Add glut-based events

--- trunk/Source/main-macosx.cpp
+++ trunk/Source/main-macosx.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 #include <AmjuGL-OpenGL.h>
-
+#include <Screen.h>
 #include <Game.h>
 #include "StartUp.h"
 #include <SoundManager.h>
@@ -15,6 +15,9 @@
 #endif
 
 #include <AmjuFinal.h>
+
+static const int WINDOW_W = 640;
+static const int WINDOW_H = 480;
 
 using namespace Amju;
 
@@ -35,6 +38,99 @@ void draw()
   glutSwapBuffers(); // ?
   glutPostRedisplay();
 }
+
+
+void QueueEvent(Amju::Event* e)
+{
+  ((Amju::EventPollerImplGeneric*)Amju::TheEventPoller::Instance()->GetImpl())->QueueEvent(e);
+}
+
+void key(char k, bool down)
+{
+  KeyEvent* ke = new KeyEvent;  
+  ke->keyType = AMJU_KEY_CHAR;
+  ke->key = k;
+  ke->keyDown = true;
+  QueueEvent(ke);
+}
+
+void keydown(unsigned char k, int, int)
+{
+  key(k, true);
+}
+
+void keyup(unsigned char k, int, int)
+{
+  key(k, false);
+}
+
+void special(int key, bool down)
+{
+  KeyEvent* ke = new KeyEvent;  
+  switch (key)
+  {
+  case GLUT_KEY_LEFT:
+    ke->keyType = AMJU_KEY_LEFT;
+    break;
+
+  case GLUT_KEY_RIGHT:
+    ke->keyType = AMJU_KEY_RIGHT;
+    break;
+
+  case GLUT_KEY_UP:
+    ke->keyType = AMJU_KEY_UP;
+    break;
+
+  case GLUT_KEY_DOWN:
+    ke->keyType = AMJU_KEY_DOWN;
+    break;
+
+  }
+  ke->keyDown = down;
+  QueueEvent(ke);
+}
+
+void specialkeydown(int key, int, int)
+{
+  special(key, true);
+}
+
+void specialkeyup(int key, int, int)
+{
+  special(key, false);
+}
+
+void mousebutton(int button, int state, int x, int y)
+{
+  MouseButtonEvent* mbe = new MouseButtonEvent;
+  switch (button)
+  {
+  case GLUT_LEFT_BUTTON:
+    mbe->button = AMJU_BUTTON_MOUSE_LEFT;
+    break;
+
+  case GLUT_RIGHT_BUTTON:
+    mbe->button = AMJU_BUTTON_MOUSE_RIGHT;
+    break;
+
+  case GLUT_MIDDLE_BUTTON:
+    mbe->button = AMJU_BUTTON_MOUSE_MIDDLE;
+    break;
+  }
+  mbe->isDown = (state == GLUT_DOWN);
+  mbe->x = (float)x / (float)Amju::Screen::X(); 
+  mbe->y = 1.0f - (float)y / (float)Amju::Screen::Y(); 
+  QueueEvent(mbe);
+}
+
+void mousemove(int x, int y)
+{
+  CursorEvent* ce = new CursorEvent;
+  ce->controller = 0;
+  ce->x = (float)x / (float)Amju::Screen::X() * 2.0f - 1.0f; 
+  ce->y = 1.0f - (float)y / (float)Amju::Screen::Y() * 2.0f; 
+  QueueEvent(ce);
+}
 #endif
 
 int main(int argc, char **argv)
@@ -43,15 +139,23 @@ int main(int argc, char **argv)
   // TODO Have a CreateWindow for GLUT.. but events are tangled up with creating windows :-(
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+  glutInitWindowSize(WINDOW_W, WINDOW_H);
   glutCreateWindow("Hello");
   glutDisplayFunc(draw);
+  glutKeyboardFunc(keydown);
+  glutKeyboardUpFunc(keyup);
+  glutSpecialFunc(specialkeydown);
+  glutSpecialUpFunc(specialkeyup);
+  glutMouseFunc(mousebutton);
+  glutMotionFunc(mousemove);
+  glutPassiveMotionFunc(mousemove);
 #endif
 
   TheEventPoller::Instance()->SetImpl(new EventPollerImplGeneric); 
 
   AmjuGL::SetImpl(new AmjuGLOpenGL);
 
-  Amju::AmjuGLWindowInfo w(640, 480, false);
+  Amju::AmjuGLWindowInfo w(WINDOW_W, WINDOW_H, false);
   Amju::AmjuGL::CreateWindow(&w);
   Amju::AmjuGL::Init();
 
