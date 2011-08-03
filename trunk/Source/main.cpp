9ba1c3e1ed3b19db31336539e958e9107d115d57
jason.e.colman@gmail.com <jason.e.colman@gmail.com@347d3d46-8993-11de-a519-b78fc8232508>
2011-08-03 12:32:56 +0000

    Fixed mising namespace

--- trunk/Source/main.cpp
+++ trunk/Source/main.cpp
@@ -1,8 +1,10 @@
-// #define AMJU_USE_GLUT - defined in makefule rules
-
 // Create global variable window info 
 #include <AmjuGLWindowInfo.h>
-Amju::AmjuGLWindowInfo w(640, 480, false);
+
+namespace Amju
+{
+AmjuGLWindowInfo w(640, 480, false);
+}
 
 #include "StartUp.h"
 #include <main.h>
