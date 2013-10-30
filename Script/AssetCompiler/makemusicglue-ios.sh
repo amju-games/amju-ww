e89140c69409119967b375d60ec2d00bc208d21f
jason.e.colman <jason.e.colman@gmail.com>
2013-10-30 13:24:14 +0000

    Add scripts to make glue files for iOS

---
+++ Script/AssetCompiler/makemusicglue-ios.sh
@@ -0,0 +1,34 @@
+# makemusicglue-ios.sh
+
+export PLATFORM=iphone
+export THIS_DIR=`pwd`
+export TOP_DIR=$THIS_DIR/../..
+export COMPILED_ASSETS=$TOP_DIR/Build/CompiledAssets
+export DEST_DIR=$COMPILED_ASSETS/$PLATFORM
+export SRC_DIR=$TOP_DIR/Assets
+export GLUE_EXE=$THIS_DIR/glue
+export GLUE_FILE=$DEST_DIR/../music-$PLATFORM.glue
+
+mkdir $COMPILED_ASSETS
+mkdir $DEST_DIR
+
+cp $SRC_DIR/sound/* $DEST_DIR
+cp $SRC_DIR/sound/windows/* $DEST_DIR
+
+cd $DEST_DIR
+
+# Create empty glue file
+$GLUE_EXE -c $GLUE_FILE
+
+for f in *.wav *.mod
+do
+  echo "Adding file: " $f
+  $GLUE_EXE -a $GLUE_FILE $f
+done
+
+# Verify glue file contents
+$GLUE_EXE -d $GLUE_FILE
+cd $THIS_DIR
+
+
+
