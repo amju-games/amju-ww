d1dc8d4ac47fea7843e1f6de736fcc5bbc4754dd
jason.e.colman <jason.e.colman@gmail.com>
2013-11-08 06:46:21 +0000

    Add rotating background to level complete state

--- Source/GSLevelComplete.cpp
+++ Source/GSLevelComplete.cpp
@@ -15,12 +15,17 @@ const char* GSLevelComplete::NAME = "level-complete";
 GSLevelComplete::GSLevelComplete()
 {
   m_timer = 0;
+  if (!m_bg.OpenAndLoad("level-complete-bg.txt"))
+  {
+    Assert(0);
+  }
+  m_z = 0;
 }
 
-void SetObjPos(WWGameObject* obj, float angle)
+void SetObjPos(WWGameObject* obj, float angle, float z)
 {
-  const float RADIUS = 80.0f;
-  Vec3f pos(cos(angle) * RADIUS, sin(angle) * RADIUS, 0);
+  static const float RADIUS = 80.0f;
+  Vec3f pos(cos(angle) * RADIUS, sin(angle) * RADIUS, z);
   obj->SetPos(pos);  
   obj->RecalcAABB();
 
@@ -36,28 +41,30 @@ void GSLevelComplete::Update()
   GetGameSceneGraph()->Update();
 
   float dt = TheTimer::Instance()->GetDt();
+  
+  static const float Z_VEL = 200.0f;
+  static const float Z_INCREMENT = 40.0f;
+
+  m_z -= Z_VEL * dt;
 
   // Circle of positions - also set y coord for sprial
   static float angle = 0; // increases per frame
   angle += DegToRad(90.0f) * dt;
 
   float a = angle;
-  SetObjPos(m_player, a);
+  SetObjPos(m_player, a, m_z);
 
   const Pets& pets = m_player->GetPets();
   for (auto it = pets.begin(); it != pets.end(); ++it)
   {
     a += DegToRad(30.0f); // TODO
     Pet* pet = const_cast<Pet*>(*it);
-    SetObjPos(pet, a);
+    SetObjPos(pet, a, m_z + Z_INCREMENT);
   }   
-
-  // Update scene nodes
-  //TheGame::Instance()->UpdateGameObjects();
 
   // Inc timer, go to next state
   m_timer += TheTimer::Instance()->GetDt();
-  if (m_timer > 50.0f) // TODO CONFIG
+  if (m_timer > 10.0f) // TODO CONFIG
   {
     // Increment level number done by "Exit" object
     TheGame::Instance()->SetCurrentState(TheGSLoadLevel::Instance());
@@ -70,6 +77,23 @@ void GSLevelComplete::Draw2d()
 
 void GSLevelComplete::Draw()
 {
+  // Draw rotating background
+  AmjuGL::Disable(AmjuGL::AMJU_DEPTH_READ);
+  AmjuGL::Disable(AmjuGL::AMJU_DEPTH_WRITE);
+
+  // Rotate background
+  static float a = 0;
+  float dt = TheTimer::Instance()->GetDt();
+  a += 90.0f * dt;
+
+  AmjuGL::PushMatrix();
+  AmjuGL::RotateZ(a);
+  m_bg.Draw(); 
+  AmjuGL::PopMatrix();
+
+  AmjuGL::Enable(AmjuGL::AMJU_DEPTH_READ);
+  AmjuGL::Enable(AmjuGL::AMJU_DEPTH_WRITE);
+
   AmjuGL::SetMatrixMode(AmjuGL::AMJU_PROJECTION_MATRIX);
   AmjuGL::SetIdentity();
   const float FOVY = 60.0f;
@@ -90,7 +114,8 @@ void GSLevelComplete::OnDeactive()
 {
   GameState::OnDeactive();
   m_player = 0; // no need to retain player any more
-  TheLevelManager::Instance()->Clear();
+  // Still need for bonus display state!
+//  TheLevelManager::Instance()->Clear();
 }
 
 void GSLevelComplete::OnActive()
@@ -99,7 +124,8 @@ void GSLevelComplete::OnActive()
   // Don't kill game objects yet, we are displaying them!
   m_timer = 0;
 
-std::cout << "Showing level complete state...\n";
+  static const float Z_START = 250.0f;
+  m_z = Z_START;
 
   // Show player and pets only
   GetGameSceneGraph()->Clear(); // ?
@@ -120,7 +146,7 @@ std::cout << "Showing level complete state...\n";
   // Set up camera
   // TODO Need member var so we can update??
   SceneNodeCamera* snc = new SceneNodeCamera;  
-  snc->SetEyePos(Vec3f(0, 0, -200.0f));
+  snc->SetEyePos(Vec3f(0, 0, 200.0f));
   snc->SetLookAtPos(Vec3f(0, 0, 0));
   GetGameSceneGraph()->SetCamera(snc);
 }
