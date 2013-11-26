c5d6244ddf7ee995199e6ee3a19925628ffa3311
jason.e.colman@gmail.com <jason.e.colman@gmail.com@347d3d46-8993-11de-a519-b78fc8232508>
2013-11-26 23:29:43 +0000

    Bird improvements

--- trunk/Source/AIFly.cpp
+++ trunk/Source/AIFly.cpp
@@ -27,18 +27,25 @@ void AIFly::Update()
 
   m_npc->SetAnim("fly"); // why not just in OnActivated?? TODO
 
-  Vec3f a = (m_target->GetPos() + Vec3f(0, 40.0f, 0)) - m_npc->GetPos();
+  Vec3f a = (m_target->GetPos() + Vec3f(0, 50.0f, 0)) - m_npc->GetPos();
   a.Normalise();
   a *= 50.0f; // TODO CONFIG
   m_npc->SetAcc(a);
 
   a = m_npc->GetVel();
-  if (a.SqLen() > 1.0f) // TODO
+  float speedSq = a.SqLen();
+  const float MAX_SPEED = 50.0f; // TODO CONFIG
+  if (speedSq > MAX_SPEED * MAX_SPEED)
   {
-    float degs = RadToDeg(atan2(a.x, a.z)); 
-    // TODO normalise degs?
-    m_npc->SetDir(degs);
+    a.Normalise();
+    a *= MAX_SPEED;
+    m_npc->SetVel(a);
   }
+
+  // TODO Work out why bird sometimes flips dir
+  float degs = RadToDeg(atan2(a.x, a.z)); 
+  // TODO normalise degs?
+  m_npc->SetDir(degs);
 }
 
 }
