9d573398814ad36b20e1c2571447b7413ba027f3
jason.e.colman <jason.e.colman@gmail.com>
2013-12-05 17:38:11 +0000

    Add Vec3f

--- Source/Describe.cpp
+++ Source/Describe.cpp
@@ -8,5 +8,10 @@ std::string Describe(const GameObject* go)
 {
   return std::string(go->GetTypeName()) + " ID: " + ToString(go->GetId());
 }
+
+std::string Describe(const Vec3f& v)
+{
+  return "(" + ToString(v.x, 2) + ", " + ToString(v.y, 2) + ", " + ToString(v.z, 2) + ")";
+}
 }
 
--- Source/Describe.h
+++ Source/Describe.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <string>
+#include <Vec3.h>
 
 namespace Amju
 {
 class GameObject;
 
 std::string Describe(const GameObject* go);
+
+std::string Describe(const Vec3f& v);
 }
