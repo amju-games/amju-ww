5497a76414f89cf7cbab1ebcc11a57a79263427b
jason.e.colman <jason.e.colman@gmail.com>
2013-12-08 20:37:46 +0000

    Describe (rotation) matrix

--- Source/Describe.cpp
+++ Source/Describe.cpp
@@ -1,5 +1,7 @@
 #include <GameObject.h>
 #include <StringUtils.h>
+#include <Quaternion.h>
+#include <DegRad.h>
 #include "Describe.h"
 
 namespace Amju
@@ -13,5 +15,16 @@ std::string Describe(const Vec3f& v)
 {
   return "(" + ToString(v.x, 2) + ", " + ToString(v.y, 2) + ", " + ToString(v.z, 2) + ")";
 }
+
+std::string Describe(const Matrix& mat)
+{
+  Quaternion q;
+  q.CreateFromMatrix(mat);
+  float rads = 0;
+  Vec3f axis;
+  q.GetAxisAngle(&rads, &axis);
+  return ToString(RadToDeg(rads), 2) + " degs around " + Describe(axis);
+}
+
 }
 
--- Source/Describe.h
+++ Source/Describe.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <Vec3.h>
+#include <Matrix.h>
 
 namespace Amju
 {
@@ -10,4 +11,7 @@ class GameObject;
 std::string Describe(const GameObject* go);
 
 std::string Describe(const Vec3f& v);
+
+std::string Describe(const Matrix& mat);
+
 }
