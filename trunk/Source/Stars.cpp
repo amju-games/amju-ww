d65b6f196fbf6e7c7a83a4a7fd3b07fc2b0ba3ef
jason.e.colman@gmail.com <jason.e.colman@gmail.com@347d3d46-8993-11de-a519-b78fc8232508>
2014-04-06 00:06:34 +0000

    Move stars effect to file so we can reuse as game bg

---
+++ trunk/Source/Stars.cpp
@@ -0,0 +1,45 @@
+#include <ParticleEffect2d.h>
+#include <AmjuRand.h>
+#include "Stars.h"
+
+namespace Amju
+{
+static int s_starId = 0;
+
+class StarEffect : public ParticleEffect2d
+{
+public:
+  virtual Vec3f NewPos() const override
+  {
+    int i = s_starId + 10;
+    s_starId++;
+
+    float r = (float)i * 0.02f; // radius
+    float angle = (float)i * 0.6f;
+    Vec3f tr(r * cos(angle), r * sin(angle), Rnd(-2.0f, -1.0f));
+    return tr;
+  }
+
+  virtual float NewRot() const
+  {
+    return Rnd(-6.0f, 6.0f);
+  }
+
+  virtual float NewRotVel() const
+  {
+    return Rnd(-2.0f, 2.0f);
+  }
+};
+
+
+SceneNode* CreateStars(const std::string& textureName, 
+    float particlesize, int numparticles, float maxTime, float minY)
+{
+  srand(0); // 2 particle effects which we want to overlap - so set seed the same twice
+  s_starId = 0;
+  StarEffect* stars = new StarEffect;
+  stars->Set(textureName, particlesize, numparticles, maxTime, minY);
+  stars->Start();
+  return stars;
+}
+}
