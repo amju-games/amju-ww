d8f1f89fd3aa541407ae37d9d5d882446b59830b
jason.e.colman <jason.e.colman@gmail.com>
2014-04-12 13:36:49 +0000

    New XCode project, with new game name and new version of XCode (5.1.1)

---
+++ Script/iOS-XCode-rainbowDrop/Rainbow Drop/Rainbow Drop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rainbow Drop
+//
+//  Created by JC on 11/04/2014.
+//  Copyright (c) 2014 Amju Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
