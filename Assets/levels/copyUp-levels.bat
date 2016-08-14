e0585b3fff021885937b80c0162c5cf9d7b03c2f
Jason Colman <jason@touchsurgery.com>
2016-08-14 10:10:57 +0100

    Update for new home PC

--- Assets/levels/copyUp-levels.bat
+++ Assets/levels/copyUp-levels.bat
@@ -1,5 +1,6 @@
 REM copy level file from repository to Save Dir for quick testing
 copy *.txt  \Users\jason\AppData\Local\amju-ww\levels\
+copy *.txt  \Users\Dad\AppData\Local\amju-ww\levels\
 
 REM Sultana
 copy *.txt "\Documents and Settings\Administrator\Local Settings\Application Data\amju-ww\levels\"
