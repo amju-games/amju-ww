5582e41141eead12cee53117951102b3235a1f2f
Jason Colman <jason@touchsurgery.com>
2016-08-23 07:40:27 +0100

    Scrolling hi scores display

--- Source/GSHiScores.h
+++ Source/GSHiScores.h
@@ -1,6 +1,6 @@
 #pragma once
 
-
+#include <GuiComposite.h>
 #include <Singleton.h>
 #include "GSText.h"
 
@@ -17,6 +17,26 @@ public:
   virtual void Draw2d();
   virtual void OnActive();
 
+private:
+  enum Mode
+  {
+    HISCORE_MODE_SMOOTH_SCROLL,
+    HISCORE_MODE_SCROLL_TO_TARGET,
+    HISCORE_MODE_USER_SCROLL
+  };
+  Mode m_mode;
+  
+  // Number of score line to scroll to, or -1 to scroll smoothly from the top.
+  // Zero-based, so 0 is the top score.
+  int m_target;
+  
+  // Currently displayed score, which we want to increment until it reaches target
+  int m_current;
+  
+  // Composite parent for all score lines
+  GuiComposite* m_comp;
+  
+  int m_numScores;
 };
 typedef Singleton<GSHiScores> TheGSHiScores;
 } // namespace
