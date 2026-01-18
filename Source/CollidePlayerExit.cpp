bf8178ca54b63aaf66f1dcc0a577ebfe92d95bbf
Juliet <juliet.e.colman@gmail.com>
2026-01-18 13:07:29 +0000

    Fix signatures for collision handler functions

--- Source/CollidePlayerExit.cpp
+++ Source/CollidePlayerExit.cpp
@@ -6,8 +6,13 @@
 
 namespace Amju
 {
-void CollidePlayerExit(Player* player, Exit* exit)
+void CollidePlayerExit(GameObject* go1, GameObject* go2)
 {
+  Player* player = dynamic_cast<Player*>(go1);
+  Assert(player);
+  Exit* exit = dynamic_cast<Exit*>(go2);
+  Assert(exit);
+
   if (exit->IsExiting())
   {
     return; // already processed this collision
@@ -27,5 +32,5 @@ void CollidePlayerExit(Player* player, Exit* exit)
 }
 
 static bool b1 = TheCollisionManager::Instance()->Add(
-  Player::NAME, Exit::NAME, (CollisionManager::CollisionHandler)CollidePlayerExit);
+  Player::NAME, Exit::NAME, CollidePlayerExit);
 } 
