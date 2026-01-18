535bca03d6f80fa7abf3d614834b8081d2e148fc
Juliet <juliet.e.colman@gmail.com>
2026-01-18 13:07:17 +0000

    Testing nested menu

--- Source/GSMainEdit.cpp
+++ Source/GSMainEdit.cpp
@@ -18,10 +18,24 @@ void GSMainEdit::OnActive()
   GSMain::OnActive();
   //GetGameSceneGraph()->SetCamera(new EditModeCamera);
 
+  GuiMenu* childMenu = new GuiMenu;
+  childMenu->AddItem(new GuiMenuItem("good"));
+  childMenu->AddItem(new GuiMenuItem("Lord"));
+  childMenu->AddItem(new GuiMenuItem("this"));
+  childMenu->AddItem(new GuiMenuItem("seems"));
+  childMenu->AddItem(new GuiMenuItem("to"));
+  childMenu->AddItem(new GuiMenuItem("work"));
+
   m_menu = new ContextMenu;
   m_menu->AddItem(new GuiMenuItem("Hello"));
   m_menu->AddItem(new GuiMenuItem("I am"));
   m_menu->AddItem(new GuiMenuItem("some text"));
+  m_menu->AddItem(new GuiNestMenuItem("I R Nested!", childMenu));
+
+  m_menu->SetName("Parent menu");
+  childMenu->SetName("Child menu");
+
+  m_menu->SetVisible(false);
 }
 
 void GSMainEdit::Update()
