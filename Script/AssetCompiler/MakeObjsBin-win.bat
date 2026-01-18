cc349c00069640a5b6147364ed2b5fe27557db08
Juliet <juliet.e.colman@gmail.com>
2026-01-18 13:07:29 +0000

    Fix creating glue file with binary objs for Win

--- Script/AssetCompiler/MakeObjsBin-win.bat
+++ Script/AssetCompiler/MakeObjsBin-win.bat
@@ -11,11 +11,13 @@ cd %SRC_DIR%
 cd obj
 REM Need this mtl file to load the text-format obj files
 copy default.mtl font3d
+copy ..\tex\common\wh8.png font3d
 
 mkdir %DEST_DIR%\obj\font3d
 cd font3d
 for %%f in (*.obj) do %RESTORE_DIR%\obj2bin %%f %DEST_DIR%\obj\font3d\%%f
 del default.mtl
+del wh8.png
 
 mkdir %DEST_DIR%\obj
 cd ..
