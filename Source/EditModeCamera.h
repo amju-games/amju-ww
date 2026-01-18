312d770fe2ad2bbce679f5b33c55cd49cf3d084b
Juliet <juliet.e.colman@gmail.com>
2026-01-18 13:07:18 +0000

    Improved camera control in Edit mode

--- Source/EditModeCamera.h
+++ Source/EditModeCamera.h
@@ -2,13 +2,25 @@
 #define EDIT_MODE_CAMERA_H
 
 #include "SceneNodeCamera.h"
+#include "EventListener.h"
 
 namespace Amju
 {
-class EditModeCamera : public SceneNodeCamera
+class EditModeCamera : public SceneNodeCamera, public EventListener
 {
 public:
+  EditModeCamera();
+  virtual ~EditModeCamera();
+
   virtual void Update();
+
+  virtual void OnCursorEvent(const CursorEvent&); 
+  virtual void OnMouseButtonEvent(const MouseButtonEvent&);
+
+private:
+  bool m_drag;
+  enum Mode { AMJU_PAN, AMJU_ROTATE, AMJU_ZOOM };
+  Mode m_mode;
 };
 }
 
