6c138506e7cd8cbb74685138a326e3e7b21280b0
Juliet <juliet.e.colman@gmail.com>
2026-01-18 13:08:39 +0000

    Ongoing fixes for tweeting

--- Source/GSTweet.cpp
+++ Source/GSTweet.cpp
@@ -1,21 +1,43 @@
 #include <Game.h>
+#include <GuiTextEdit.h>
 #include "GSTweet.h"
 #include "iOSKeyboard.h"
+#include "iOSTweet.h"
 #include "WWLoadGui.h"
 
 namespace Amju
 {
-// Call from other states
+static std::string s_defaultText;
+
+// Call from other states -- NOT static
 void OnTweet(GuiElement*)
 {
-  GSTweet* g = TheGSTweet::Instance();
-  g->SetPrevState(TheGame::Instance()->GetState());
-  TheGame::Instance()->SetCurrentState(g);
+  GSTweet* tweet = TheGSTweet::Instance();
+  auto* current = TheGame::Instance()->GetState();
+  Assert(current != tweet); // how did this happen?
+  tweet->SetPrevState(current);
+  TheGame::Instance()->SetCurrentState(tweet);
+}
+
+// Actually send the tweet
+static void OnSendTweet(GuiElement*)
+{
+  TheGSTweet::Instance()->OnSendTweet();
+}
+  
+static void OnTweetTextChanged(GuiElement*)
+{
+  TheGSTweet::Instance()->UpdateCharCount();
 }
 
 static void OnBack(GuiElement*)
 {
   TheGSTweet::Instance()->GoBack();
+}
+  
+void GSTweet::SetDefaultText(const std::string& text)
+{
+  s_defaultText = text;
 }
 
 GSTweet::GSTweet()
@@ -37,6 +59,26 @@ void GSTweet::Draw2d()
   GSText::Draw2d();
 }
 
+void GSTweet::UpdateCharCount()
+{
+  IGuiTextEdit* textEdit = dynamic_cast<IGuiTextEdit*>(GetElementByName(m_gui, "edit-tweet"));
+  Assert(textEdit);
+  int numCh = static_cast<int>(textEdit->GetText().size());
+  IGuiText* charCount = dynamic_cast<IGuiText*>(GetElementByName(m_gui, "char-count"));
+  Assert(charCount);
+  numCh = 160 - numCh;
+  charCount->SetText(ToString(numCh));
+  // TODO Different colour if low?
+}
+  
+void GSTweet::OnSendTweet()
+{
+  IGuiTextEdit* textEdit = dynamic_cast<IGuiTextEdit*>(GetElementByName(m_gui, "edit-tweet"));
+  Assert(textEdit);
+
+  TwitterPostMessage(0, textEdit->GetText());
+}
+  
 void GSTweet::OnActive()
 {
   GSText::OnActive();
@@ -44,18 +86,25 @@ void GSTweet::OnActive()
   m_gui = WWLoadGui("gui-tweet.txt");
   Assert(m_gui);
 
-  // TODO Set focus element, cancel element, command handlers
   GetElementByName(m_gui, "back-button")->SetCommand(OnBack);
-  GetElementByName(m_gui, "tweet-button")->SetCommand(OnTweet);
+  GetElementByName(m_gui, "tweet-button")->SetCommand(Amju::OnSendTweet);
   
-  // TODO Get number of accounts. If zero, we shouldn't be here.
-  // If one, hide accounts button and centre back button.
+  // TODO Get number of accounts. If zero, we shouldn't be here?
+  // OR Show a message, e.g. "Please log in to your Twitter account using the Twitter app"
+  // If one account, hide accounts button and centre back button.
   // Accounts button goes to account select page, which comes back here.
+  // Save/restore most recently used account in config.
+  
+  IGuiTextEdit* textEdit = dynamic_cast<IGuiTextEdit*>(GetElementByName(m_gui, "edit-tweet"));
+  Assert(textEdit);
+  textEdit->SetOnChangeFunc(OnTweetTextChanged);
+  textEdit->SetText(s_defaultText);
   
 #ifdef AMJU_IOS
-  GuiTextEditIos* textEdit = (GuiTextEditIos*)GetElementByName(m_gui, "edit-tweet");
-  textEdit->ShowKeyboard(true);
+  dynamic_cast<GuiTextEditIos*>(textEdit)->ShowKeyboard(true);
 #endif
+  
+  UpdateCharCount();
 }
   
 void GSTweet::OnDeactive()
