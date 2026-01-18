bee0a9a6ff2295ef3873e8ca28d4ec34dabf218d
Juliet <juliet.e.colman@gmail.com>
2026-01-18 13:07:10 +0000

    Tweak scripts to compile assets

---
+++ Script/AssetCompiler/make-everything-win.bat
@@ -0,0 +1,10 @@
+REM Convert obj files to binary format, copy to compiled dir
+call MakeObjsBin-win.bat
+
+REM Make data.glue
+call makeglue-win.bat
+
+REM Now make music glue file
+rem call makemusicglue-win.bat
+
+
