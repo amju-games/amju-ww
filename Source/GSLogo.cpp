512a158d6f3467d42a23543beeaaa314d9d8a38a
Juliet <juliet.e.colman@gmail.com>
2026-01-18 13:06:56 +0000

    Add resource groups

--- Source/GSLogo.cpp
+++ Source/GSLogo.cpp
@@ -5,9 +5,12 @@
 #include "Colour.h"
 #include "GSTitle.h"
 #include "SoundManager.h"
+#include "ResourceManager.h"
 
 namespace Amju
 {
+static const char* GROUP = "logo-group";
+
 const char* GSLogo::NAME = "start";
 
 static bool b = TheGame::Instance()->AddState(GSLogo::NAME, new GSLogo);
@@ -25,11 +28,14 @@ GSLogo::GSLogo()
 void GSLogo::OnActive()
 {
   GameState::OnActive();
+  TheResourceManager::Instance()->LoadResourceGroup(GROUP);
   m_image.OpenAndLoad("splash-gui.txt");
+}
 
-  // TODO TEMP TEST
-  TheSoundManager::Instance()->PlayWav("churchbell"); // NB No file ext
-
+void GSLogo::OnDeactive()
+{
+  GameState::OnDeactive();
+  TheResourceManager::Instance()->FreeResourceGroup(GROUP);
 }
 
 void GSLogo::Update()
