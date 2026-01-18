8c7e75a7cfacaeccbe6ae474fbb2b0cc3ba6d69a
Juliet <juliet.e.colman@gmail.com>
2026-01-18 13:08:14 +0000

    Improvements for scrolling design

--- Source/Depth.cpp
+++ Source/Depth.cpp
@@ -1,14 +1,28 @@
 #include <AmjuAssert.h>
 #include <Timer.h>
+#include <ROConfig.h>
 #include "Depth.h"
 
 namespace Amju
 {
 static float s_depth = 0;
+static float s_scrollSpeed = 1.0f;
+
+float GetScrollSpeed()
+{
+  return s_scrollSpeed;
+}
+
+void SetScrollSpeed(float ss)
+{
+  Assert(ss >= 0); // negative would mean scroll up - not allowed, right??
+  s_scrollSpeed = ss;
+}
 
 void ResetDepth()
 {
   s_depth = 0;
+  s_scrollSpeed = ROConfig()->GetFloat("scroll-speed");
 }
 
 float GetCurrentDepth()
@@ -25,11 +39,10 @@ void IncreaseDepth(float depth)
 void DepthUpdate(float minDepth)
 {
   // New game design: camera always moves down
-  float scrollSpeed = 50.0f; // TODO speed up ?
 
   float dt = TheTimer::Instance()->GetDt();
 
-  float d = dt * scrollSpeed;
+  float d = dt * s_scrollSpeed;
 
   IncreaseDepth(d);
 
