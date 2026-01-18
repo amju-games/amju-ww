9648f5e8284c247afe2043d552e170d2aad07405
Juliet <juliet.e.colman@gmail.com>
2026-01-18 13:08:23 +0000

    New XCode project, with new game name and new version of XCode (5.1.1)

---
+++ Script/iOS-XCode-rainbowDrop/Rainbow Drop/Rainbow Drop/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Rainbow Drop
+//
+//  Created by JC on 11/04/2014.
+//  Copyright (c) 2014 Amju Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
