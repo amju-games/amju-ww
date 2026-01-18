2fa30233e9c3268f3759f62bfe0476527dc1d583
Juliet <juliet.e.colman@gmail.com>
2026-01-18 13:07:08 +0000

    Add AI classes to NPCs

--- Source/Npc.h
+++ Source/Npc.h
@@ -2,14 +2,26 @@
 #define NPC_H_INCLUDED
 
 #include "OnFloorCharacter.h"
+#include "AI.h"
+#include <map>
 
 namespace Amju
 {
 class Npc : public OnFloorCharacter
 {
 public:
+  Npc();
+  void SetAI(const char* aiName);
+  void SetAI(AI*);
+
+  void AddAI(AI*); // call to populate map
+  void DecideAI();
+  virtual void Update();
 
 protected:
+  typedef std::map<std::string, PAI> AIs;
+  AIs m_ais;
+  AI* m_ai;
 };
 }
 
